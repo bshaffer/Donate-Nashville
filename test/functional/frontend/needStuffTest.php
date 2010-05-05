2696ab719836a8a82dd346f0d8b0baf860b2bffc
Lacy Rhoades <lacyrhoades@gmail.com>
2010-05-05 18:46:37 -0500

    [tests] added fuller walkthrough to need-stuff-test

--- test/functional/frontend/needStuffTest.php
+++ test/functional/frontend/needStuffTest.php
@@ -12,4 +12,20 @@ $browser
     
   ->click('Stuff')
     ->isModuleAction('stuff', 'need')
+    
+  ->call('/stuff/list', 'post', $parameters = array('q'=>'pump'))
+    ->isModuleAction('resource', 'stuffList')
+    
+  ->with('response')->begin()
+    ->matches('/sump/i')
+  ->end()
+    
+  ->click('Sump Pump')
+    ->isModuleAction('stuff', 'match')
+    
+  ->with('response')->begin()
+    ->matches('/1313 N. 4th Ave/')
+    ->matches('/Contact This User/')
+  ->end()
+  
 ;
