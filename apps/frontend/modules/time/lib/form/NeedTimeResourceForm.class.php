48e311e06514a62092dbe088f5cd6d53dca4044b
Adam Kimbrel <adam@adam-eee.(none)>
2010-05-04 21:59:22 -0500

    need time form coming along

--- apps/frontend/modules/time/lib/form/NeedTimeResourceForm.class.php
+++ apps/frontend/modules/time/lib/form/NeedTimeResourceForm.class.php
@@ -5,5 +5,32 @@ class NeedTimeResourceForm extends TimeResourceForm
   public function configure()
   {
     parent::configure();
+    
+    $this->useFields(array(
+      'start_date',
+      'end_date',
+      'title',
+      'address_1',
+      'address_2',
+      'city',
+      'state',
+      'zip',
+      'description',
+      'num_volunteers',
+      'email',
+      'phone_1',
+      'phone_2',
+      'show_contact_info'
+    ));
+  }
+  
+  public function doSave($con = null)
+  {
+    $email = $this->getValue('email');
+    $user = sfContext::getInstance()->getUser();
+    
+    $this->User = $user->getGuardUserByEmail($email);
+    
+    parent::doSave($con);
   }
 }
--- apps/frontend/modules/time/templates/needSuccess.php
+++ apps/frontend/modules/time/templates/needSuccess.php
@@ -2,7 +2,110 @@
   <?php echo $form->renderGlobalErrors() ?>
   <?php echo $form->renderHiddenFields() ?>
   
-  <?php //echo $form['']->render() ?>
+  <div>
+    <div style="float:left;">
+      When
+    </div>
+    <div style="float:left;">
+      <?php echo $form['start_date']->renderError() ?>
+      <?php echo $form['start_date']->render() ?>
+      -
+      <?php echo $form['end_date']->renderError() ?>
+      <?php echo $form['end_date']->render() ?> 
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      What
+    </div>
+    <div style="float:left;">
+      <?php echo $form['title']->renderError() ?>
+      <?php echo $form['title']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      Where
+    </div>
+    <div style="float:left;">
+      <?php echo $form['address_1']->renderError() ?>
+      <?php echo $form['address_1']->render() ?>
+      <br />
+      <?php echo $form['address_2']->renderError() ?>
+      <?php echo $form['address_2']->render() ?>
+      <br />
+      <?php echo $form['city']->renderError() ?>
+      <?php echo $form['city']->render() ?>
+      <br />
+      <?php echo $form['state']->renderError() ?>
+      <?php echo $form['state']->render() ?>
+      <br />
+      <?php echo $form['zip']->renderError() ?>
+      <?php echo $form['zip']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      Description
+    </div>
+    <div style="float:left;">
+      <?php echo $form['description']->renderError() ?>
+      <?php echo $form['description']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      # of Volunteers
+    </div>
+    <div style="float:left;">
+      <?php echo $form['num_volunteers']->renderError() ?>
+      <?php echo $form['num_volunteers']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      Contact Info
+    </div>
+    <div style="float:left;">
+      <?php echo $form['email']->renderError() ?>
+      <?php echo $form['email']->render() ?>
+      <br />
+      <?php echo $form['phone_1']->renderError() ?>
+      <?php echo $form['phone_1']->render() ?>
+      <br />
+      <?php echo $form['phone_2']->renderError() ?>
+      <?php echo $form['phone_2']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
+  
+  <div>
+    <div style="float:left;">
+      Privacy
+    </div>
+    <div style="float:left;">
+      <?php echo $form['show_contact_info']->renderError() ?>
+      <?php echo $form['show_contact_info']->render() ?>
+    </div>
+  </div>
+  
+  <br style="clear:both;" />
   
   <input type="submit" value="submit" />
 </form>
