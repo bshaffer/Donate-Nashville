6a1adf32eafbd26419ceadc78be0473d69047b57
Brent Shaffer <bshafs@gmail.com>
2010-05-04 20:55:31 -0500

    test fix

--- test/unit/ResourceTableTest.php
+++ test/unit/ResourceTableTest.php
@@ -2,7 +2,7 @@
 
 include(dirname(__FILE__) . '/../bootstrap/Doctrine.php');
 
-$t = new lime_test(2, new lime_output_color());
+$t = new lime_test(1, new lime_output_color());
 
 $stuff = new StuffResource();
 $stuff['title'] = 'Stuff Resource ' . csFactory::generate();
