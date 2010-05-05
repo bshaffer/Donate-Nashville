e3ad9b008029821c72c96721ec4fecf57bf27c54
Adam Kimbrel <adam@adam-eee.(none)>
2010-05-04 20:19:21 -0500

    have and need forms fleshed out a bit

--- apps/frontend/modules/time/templates/needSuccess.php
+++ apps/frontend/modules/time/templates/needSuccess.php
@@ -1,1 +1,8 @@
-<?php echo $form ?>
+<form action="<?php echo url_for('@need_time') ?>" method="post" name="time_resource">
+  <?php echo $form->renderGlobalErrors() ?>
+  <?php echo $form->renderHiddenFields() ?>
+  
+  <?php //echo $form['']->render() ?>
+  
+  <input type="submit" value="submit" />
+</form>
