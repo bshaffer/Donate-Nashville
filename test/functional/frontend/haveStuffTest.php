5e856606dad049ca058859466ebdd2e0073d96e8
Brent Shaffer <bshafs@gmail.com>
2010-05-05 18:26:33 -0500

    fixes test

--- test/functional/frontend/haveStuffTest.php
+++ test/functional/frontend/haveStuffTest.php
@@ -7,7 +7,7 @@ $browser = new dnTestFunctional(new sfBrowser());
 $browser
   ->get('/')
   
-  ->click('Have')
+  ->click('I Have...')
     ->isModuleAction('resource', 'have')
     
   ->click('Stuff')
