611b0e30b608a4449dbfab7f697e702040cfbf94
Brent Shaffer <bshafs@gmail.com>
2010-05-04 19:32:45 -0500

    switches modules according to new nomenclature

---
+++ apps/frontend/modules/time/actions/actions.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * time actions.
+ *
+ * @package    skeleton
+ * @subpackage time
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class timeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+
+  }
+  
+  public function executeNeed(sfWebRequest $request)
+  {
+
+  }
+  
+  public function executeHave(sfWebRequest $request)
+  {
+
+  }
+}
