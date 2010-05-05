0387c9860b0a9e846e04411ead86cba8ea290e9f
Devon Weller <dweller@devonweller.com>
2010-05-04 23:12:38 -0500

    finished placeholder methods for resource filter

--- web/js/app/ResourceFilter.js
+++ web/js/app/ResourceFilter.js
@@ -10,7 +10,19 @@ ResourceFilter = $.extend({}, {
 	NO_RESULTS_STRING: '[ no results found ]',
 	resource_type: 'time', // stuff or time
 	
+	setResourceType: function(resource_type) {
+		this.resource_type = resource_type;
+	},
+
 	attachEvents: function() {
+		if (this.resource_type == 'stuff') {
+			this.attachEventsForStuff();
+		} else {
+			this.attachEventsForTime();
+		}
+	},
+	
+	attachEventsForStuff: function() {
 		// keep form from submitting
 		$('.resourceFilter').submit(function(e) { e.preventDefault(); });
 		
@@ -18,18 +30,13 @@ ResourceFilter = $.extend({}, {
 		this.initSearchFilterForInput($('input.resource'));
 	},
 
-	setResourceType: function(resource_type) {
-		this.resource_type = resource_type;
-	},
-
 	// attaches the handlers to the input that send the ajax request
 	initSearchFilterForInput: function(jq_input) {
 		var self = this;
-		jq_input.keyup($.debounce(500, function() { self.runFilter(jq_input); }));
-
+		jq_input.keyup($.debounce(500, function() { self.runFilterForStuff(jq_input); }));
 	},
 
-	runFilter: function(jq_input) {
+	runFilterForStuff: function(jq_input) {
 		// console.log('runFilter called');
 		var self = this;
 
@@ -59,6 +66,55 @@ ResourceFilter = $.extend({}, {
 			self.showNoResults();
 		}
 	},
+
+
+
+	////////////////////////////////////////////////////////////
+	/// Time
+
+	attachEventsForTime: function() {
+		// unimplemented
+		
+		// here we will pull the start date, the end date and send the appropriate request
+	},
+
+	runFilterForTime: function() {
+		// get start and end times
+
+		
+		// console.log('runFilter called');
+		var self = this;
+
+		// mark as loading
+		self.updateUI(jq_input, true);
+	
+		var value = $.trim(jq_input.val());
+		if (value.length >= this.MINIMUM_FILTER_LENGTH) {
+			var action = jq_input.closest('form').attr('action')+'';
+			var vars = {start: start, end: end};
+			$('#ResourceResultsList').load(action, vars, function(data, status) {
+				// mark as not loading
+				self.updateUI(jq_input, false);
+				
+				// if we got a blank string, show the no results string instead
+				if (data.length < 1) {
+					self.showNoResults();
+				}
+			}, 'html');
+		} else {
+			// didn't get enough characters
+
+			//  show as not loading
+			self.updateUI(jq_input, false);
+			
+			// fill no results string
+			self.showNoResults();
+		}
+	},
+
+
+	////////////////////////////////////////////////////////////
+	/// Common
 
 	showNoResults: function() {
 		$('#ResourceResultsList').html('<span class="noResults">'+this.NO_RESULTS_STRING+'</span>');
