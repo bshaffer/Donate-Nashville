4901302d6273fdc9c3c9fbaabb296ac4be692d20
Brent Shaffer <bshafs@gmail.com>
2010-05-05 23:37:27 -0500

    fixes conditional on time show

--- apps/frontend/modules/time/templates/showSuccess.php
+++ apps/frontend/modules/time/templates/showSuccess.php
@@ -6,9 +6,9 @@
   <?php include_partial('user/contact_info', array('user' => $resource['User'])) ?>
 </div>
 
-<?php if ($resource['is_fulfilled'] || $sf_user->isOwner($resource)): ?>
+<?php if ($resource['is_fulfilled']): ?>
   <span class="fulfilled"><?php echo image_tag('/sfDoctrinePlugin/images/tick.png', array('alt' => 'fulfilled')) ?>&nbsp;Fulfilled</span>
-<?php else: ?>
+<?php elseif(!$sf_user->isOwner($resource)): ?>
   <!-- call to action here, which depends on whether this is a "need" looking for a "have" or vice versa -->
   <?php include_partial('resource/resource_contact_owner_form', array('form' => $form, 'type' => $type)) ?>
 <?php endif ?>
