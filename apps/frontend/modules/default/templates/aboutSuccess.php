3e8501e13920d164397af27b5954f3b306d9d280
Chip Hayner <chayner@centresource.com>
2010-05-06 12:12:58 -0500

    Added initial theming of homepage and need page

--- apps/frontend/modules/default/templates/aboutSuccess.php
+++ apps/frontend/modules/default/templates/aboutSuccess.php
@@ -1,3 +1,31 @@
-<h1>About</h1>
+<h1>Donate Nashville</h1>
+<h2>Connecting people in need with neighbors who care</h2>
 
-<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>When disaster strikes - large or small - there will always be a need that falls through the cracks:
+  a donation that doesn't make it to the right spot, or a concerned volunteer who can't find a place
+  to plug in. That's where we come in. Donate Nashville connects real needs, no matter how small or
+  unusual, with practical, community-based solutions. As neighbors, we'll work together in creative
+  and personal ways to touch each other's lives and solve our city's most pressing challenges.</p>
+
+<h3>Background: Nashville's response to the flooding of 2010</h3>
+
+<p>Nashville is suffering the aftermath of what has been labeled a "100-, 500- or 1,000-year flood," after
+more than 16 inches of rain fell over a 48-hour period. While we're still calculating the extent of the
+damage, this website represents an effort to take quick action to organize needs and resources.</p>
+
+<p>Shortly after the catastrophic flooding in New Orleans following Hurricane Katrina, some forward-thinking
+people quickly threw together a website that matched donors, housing and other resources directly with
+people in need. Inspired by that action, we have created Donate Nashville, a similar organizational tool
+for putting people and their resources in touch with their neighbors. Think of it as a peer-to-peer charitable utility.</p>
+
+<p>Nothing on this site is intended to overlap or conflict with the amazing efforts of local and national
+charities such as Hands on Nashville (HON), the Red Cross and countless other organizations that during
+this crisis have mobilized on a massive scale. Please continue to utilize and support those organizations.</p>
+
+<h3>About Us</h3>
+
+<p>Donate Nashville has been a collaborative effort among Nashville's local Symfony Group, web developers,
+strategists and, well, basically anyone else who showed up. We came together around the same time our
+community rallied around the phrase "We are Nashville."</p>
+
+<p>For non-resource-related inquires about Donate Nashville and partnering opportunities, contact <a href="mailto:&#x69;&#x6E;&#x66;&#x6F;&#x40;&#x64;&#x6F;&#x6E;&#x61;&#x74;&#x65;&#x6E;&#x61;&#x73;&#x68;&#x76;&#x69;&#x6C;&#x6C;&#x65;&#x2E;&#x63;&#x6F;&#x6D;">info@donatenashville.com</a>.</p>
