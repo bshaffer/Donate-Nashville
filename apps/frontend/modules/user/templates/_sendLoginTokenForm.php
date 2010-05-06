5454e42f5ac26389e469ef211b2d9ed412ce2a84
Brandon Valentine <brandon@brandonvalentine.com>
2010-05-06 18:50:58 -0500

    * Prettify login form * Make contact form full page again

--- apps/frontend/modules/user/templates/_sendLoginTokenForm.php
+++ apps/frontend/modules/user/templates/_sendLoginTokenForm.php
@@ -2,11 +2,19 @@
   <?php echo $form->renderGlobalErrors() ?>
   <?php echo $form->renderHiddenFields() ?>
 
-  <?php echo $form['email']->renderLabel() ?>
-  <?php echo $form['email']->renderError() ?>
-  <?php echo $form['email']->render() ?>
+<fieldset>
+  <ul>
+    <li>
+      <label><?php echo $form['email']->renderLabel() ?></label>
+      <div>
+        <?php echo $form['email']->renderError() ?>
+        <?php echo $form['email']->render() ?>
+      </div>
+    </li>
 
-  <input type="submit" value="email me the link" />
+    <li><input type="submit" value="email me the link" /></li>
+  </ul>
+</fieldset>
 </form>
 
 <script type="text/javascript">
