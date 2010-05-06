4493a3faf3d69b588994280f7e3c0eec632d7245
Ryan Weaver <ryan@thatsquality.com>
2010-05-05 23:15:24 -0500

    Removing "state" from the TimeResource form - we'll only need time for events here in TN. Made the state field use the state selector for normal Resource forms.

--- lib/form/doctrine/TimeResourceForm.class.php
+++ lib/form/doctrine/TimeResourceForm.class.php
@@ -16,5 +16,20 @@ class TimeResourceForm extends BaseTimeResourceForm
   public function configure()
   {
     parent::configure();
+    
+    // unset state, people will only need help in TN
+    unset(
+      $this['state']
+    );
+  }
+
+  /**
+   * Set TN as the state, just so things are kosher
+   */
+  public function doUpdateObject($values)
+  {
+    $values['state'] = 'TN';
+    
+    parent::doUpdateObject($values);
   }
 }
