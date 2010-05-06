444bbb5f8fb03df578757b9c47bfb7dcbe7e18ab
Brent Shaffer <bshafs@gmail.com>
2010-05-05 23:33:48 -0500

    adds better filtering for time-related events

--- apps/frontend/modules/resource/templates/_time_filter_form.php
+++ apps/frontend/modules/resource/templates/_time_filter_form.php
@@ -9,24 +9,12 @@
 
 	<ul>
 		<li>
-			<label for="date">Date</label>
-		  <?php echo $form['resource_date']->render(); ?>
+			<label for="date">Start</label>
+		  <?php echo $form['start']->render(); ?>
 		</li>
 		<li>
-		  <a href="#" onclick="javascript:$('.time-form').css('visibility', 'visible'); $(this).hide(); return false;">Specify Time</a>
-		  <div class="time-form" style="visibility:hidden">
-			  <label for="time">Time</label>
-  		  <ul>
-      		<li>
-      			<label for="start">From</label>
-      		  <?php echo $form['start_time']->render(); ?>
-      		</li>
-      		<li>
-      			<label for="start">To</label>
-      		  <?php echo $form['end_time']->render(); ?>
-      		</li>
-  		  </ul>
-		  </div>
+			<label for="date">End</label>
+		  <?php echo $form['end']->render(); ?>
 		</li>
 	</ul>
 	</fieldset>
