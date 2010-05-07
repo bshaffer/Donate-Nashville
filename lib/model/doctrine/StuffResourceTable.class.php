9967d9e5951ce1e4ebb60a89117bb50aabd37972
Brent Shaffer <bshafs@gmail.com>
2010-05-07 16:46:04 -0500

    wraps "Stuff List" query in parentheses

--- lib/model/doctrine/StuffResourceTable.class.php
+++ lib/model/doctrine/StuffResourceTable.class.php
@@ -10,7 +10,8 @@ class StuffResourceTable extends ResourceTable
     $query = $this->createQuery('p')
               ->select('p.title, LEFT(p.description, 200) as summary, p.neighborhood, p.quantity, p.created_at')
               ->andWhere('title like ?', "%$search%")
-              ->orWhere('description like ?', "%$search%");
+              ->orWhere('description like ?', "%$search%")
+              ->whereWrap();
     
     return $query;
   }
