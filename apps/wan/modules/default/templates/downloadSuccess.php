9c8bc70ff5a2dcae5026cca31fbe99fd6c5b549e
Chip Hayner <chayner@centresource.com>
2010-05-10 13:56:33 -0500

    Fixed links on the download page

--- apps/wan/modules/default/templates/downloadSuccess.php
+++ apps/wan/modules/default/templates/downloadSuccess.php
@@ -11,19 +11,19 @@
 </div>
 <ul id="downloads">
   <li id="avatar">
-    <div class="image"><a href="images/wearenashville-square.jpg"><img src="../images/wan/wearenashville-square.jpg" width="75" height="75"></a></div>
+    <div class="image"><a href="../images/wan/wearenashville-square.jpg"><img src="../images/wan/wearenashville-square.jpg" width="75" height="75"></a></div>
     <p>Twitter Avatar <span>(250px x 250px)</span></p>
     <p><a href="../images/wan/wearenashville-square.jpg">Download</a></p>
   </li>
   <li id="facebook-profile">
-    <div class="image"><a href="images/wearenashville-facebook.jpg"><img src="../images/wan/wearenashville-facebook.jpg" width="120" height="160" /></a></div>
+    <div class="image"><a href="../images/wan/wearenashville-facebook.jpg"><img src="../images/wan/wearenashville-facebook.jpg" width="120" height="160" /></a></div>
     <p>Facebook Profile Image <span>(300px x 400px)</span></p>
     <p>
       <a href="../images/wan/wearenashville-facebook.jpg">Download</a>
     </p>
   </li>
   <li id="web">
-    <div class="image"><a href="images/wearenashville-web.jpg"><img src="../images/wan/wearenashville-web.jpg" width="195" height="255"></a></div>
+    <div class="image"><a href="../images/wan/wearenashville-web.jpg"><img src="../images/wan/wearenashville-web.jpg" width="195" height="255"></a></div>
     <p>Higher Resolution Image <span>(650px x 850px)</span></p>
     <p><a href="../images/wan/wearenashville-web.jpg">Download</a></p>
   </li>
