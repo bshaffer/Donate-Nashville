cec022183f6ab853f26f4f47cf2cbbb4847aba38
Brent Shaffer <bshafs@gmail.com>
2010-05-29 10:32:16 -0500

    fixing email system: adds proper email view and information

---
+++ apps/frontend/modules/stuff/templates/matchEmailSuccess.php
@@ -0,0 +1,30 @@
+<h1>We have a match for "<?php echo $resource['title'] ?>"!</h1>
+
+<p>
+  User <?php echo $contact['name'] ?> says they <?php echo $senderAction ?> what you <?php echo $receiverAction ?>!
+</p>
+
+<p>You can contact them using the information below:</p>
+
+<dl>
+  <?php if ($contact['email']): ?>
+    <dt><strong>Email:</strong></dt>
+    <dd><?php echo $contact['email'] ?></dd>
+  <?php endif ?>
+
+  <?php if ($contact['phone']): ?>
+    <dt><strong>Phone:</strong></dt>
+    <dd><?php echo $contact['phone'] ?></dd>    
+  <?php endif ?>
+  
+  <?php if ($contact['notes']): ?>
+    <dt><strong>Add'l Info:</strong></dt>
+    <dd><?php echo $contact['notes'] ?></dd>    
+  <?php endif ?>
+
+</dl>
+
+<p>
+  Click the link below to edit, remove, or mark your item as fulfilled:
+  <br /><?php echo url_for('@user_auth?token='.$resource->User->password, array('absolute' => true)); ?>
+</p>
