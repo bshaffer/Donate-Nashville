c4c5752b25308e8ebd1b209a8dbd3df1895f7154
Stephen Yeargin <syeargin@centresource.com>
2011-01-28 18:49:04 -0600

    Changing language in pop-up notification.

--- apps/frontend/modules/default/templates/_homepagePopUp.php
+++ apps/frontend/modules/default/templates/_homepagePopUp.php
@@ -1,7 +1,7 @@
 <h1>Thanks for stopping by!</h1>
-<p>A team of 35+ amazing people have been working hard in the past 96 hours to 
-  put together this helpful resource for Nashville Flood Relief.</p>
-<p><strong>Now we need your help to get it going.</strong></p>
+<p>A team of 35+ amazing people have worked hard to put together
+  this helpful resource for Nashville Flood Relief.</p>
+<p><strong>Now we need your help to keep it going.</strong></p>
 <p>If you have suggestions on what would make this site better,
   please click the "feedback" tab to the left, and add a comment.
   We'll be checking this on an ongoing basis.</p>
